e4d5ae6fd35bda67a3192fc681f5fe52e4e10b28
Kyungtae Lee <mlogue76@gmail.com>
2020-04-23 15:31:57 +0900

    change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const os = require('os')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 
 app.get('/', function(req, res){
     res.send("Hello World from `" + os.hostname() + "`")
